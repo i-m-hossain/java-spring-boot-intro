453bce4ad283c7288adb34feb3ccd455c508dc05
Imran Hossain <im.hossain@blamanagedservices.com>
2023-06-14 16:46:02 +0600

    update and delete content api added

--- src/main/java/com/imran/example/learningspringboot/controller/ContentController.java
+++ src/main/java/com/imran/example/learningspringboot/controller/ContentController.java
@@ -5,31 +5,57 @@ import com.imran.example.learningspringboot.repository.ContentCollectionReposito
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
-
 import java.util.List;
 import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/content")
+@CrossOrigin
 public class ContentController {
     private final ContentCollectionRepository repository;
 
     public ContentController(ContentCollectionRepository repository) {
         this.repository = repository;
     }
+
+    /* --GET ALL THE CONTENTS-- */
     @GetMapping("")
     public List<Content> fetchAllContents(){
         return repository.findAll();
     }
 
-
+    /* --GET SINGLE CONTENT-- */
     @GetMapping("/{id}")
     public Content getContentById(@PathVariable Integer id){
         return repository.findById(id).orElseThrow(()->new ResponseStatusException(HttpStatus.NOT_FOUND, "Content Not Found"));
     }
-    //    @PostMapping ("")
-//    public Optional<Content> insertContent(@ResponseBody Content contentBody){
-//        return repository.insertContent(contentBody);
-//    }
+
+    /* --Insert content-- */
+    @ResponseStatus(HttpStatus.CREATED) //send 201 status
+    @PostMapping("")
+    public Content insertContent(@RequestBody Content contentBody){
+        repository.insertOne(contentBody);
+        return contentBody;
+    }
+
+    /* --Update content-- */
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @PutMapping("/{id}")
+    public void updateContent(@RequestBody Content contentBody, @PathVariable Integer id){
+        if(!repository.existsById(id)){
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Content Not Found");
+        }
+        repository.updateOne(contentBody, id);
+    }
+
+    /* --delete content-- */
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("/{id}")
+    public void  deleteContent(@PathVariable Integer id){
+        if(!repository.existsById(id)){
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Content Not Found");
+        }
+         repository.deleteOne(id);
+    }
 
 }
--- src/main/java/com/imran/example/learningspringboot/repository/ContentCollectionRepository.java
+++ src/main/java/com/imran/example/learningspringboot/repository/ContentCollectionRepository.java
@@ -6,6 +6,7 @@ import com.imran.example.learningspringboot.model.Type;
 import jakarta.annotation.PostConstruct;
 import org.springframework.stereotype.Repository;
 
+
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
@@ -14,26 +15,41 @@ import java.util.Optional;
 
 @Repository
 public class ContentCollectionRepository {
-    private final List<Content> content = new ArrayList<>();
+    private final List<Content> contentList = new ArrayList<>();
     public ContentCollectionRepository(){
 
     }
     public List<Content> findAll(){
-        return content;
+        return contentList;
     }
     public Optional<Content> findById(Integer id){
-        return content.stream().filter(c-> c.id().equals(id)).findFirst();
+        return contentList.stream().filter(c-> c.id().equals(id)).findFirst();
     }
 
-//    public Optional<Content>insertContent(Content contentBody){
-//        return content.add(contentBody);
-//    }
+    public void insertOne(Content contentBody){
+//        System.out.println(contentBody);
+         contentList.add(contentBody);
+    }
+    public void updateOne(Content contentBody, Integer id){
+         contentList.removeIf(c-> c.id().equals(id));
+         contentList.add(contentBody);
+    }
+
+    public void deleteOne(Integer id){
+        contentList.removeIf(c-> c.id().equals(id));
+    }
+
     @PostConstruct
     private void init(){
         Content content1 = new Content(1,  "this is title", "this is description", Status.IDEA, Type.ARTICLE, LocalDateTime.now(), null,  "");
         Content content2 = new Content(2,  "this is title2", "this is description2", Status.IDEA, Type.ARTICLE, LocalDateTime.now(), null,  "");
-        content.add(content1);
-        content.add(content2);
+        contentList.add(content1);
+        contentList.add(content2);
     }
+    public boolean existsById(Integer id){
+        return contentList.stream().filter(c-> c.id().equals(id)).count()==1;
+    }
+
+
 
 }
