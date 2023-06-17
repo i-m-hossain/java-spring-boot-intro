421dccea870ef42843f7c8a607d520bcbd8e7e8d
mdimran1409036 <mdimran1409036@gmail.com>
2023-06-17 20:53:30 +0600

    configuration added  for postgresql

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS Content (
-    id INT AUTO_INCREMENT PRIMARY KEY,
+    id  SERIAL PRIMARY KEY,
     title VARCHAR(255) NOT NULL,
     description TEXT,
     status VARCHAR(20) NOT NULL,
